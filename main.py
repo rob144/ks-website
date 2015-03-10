29777334841d8745d6051cab14f622ddab03e768
Robin Dunn <rob144@server.fake>
2015-03-10 00:53:11 -0400

    Initial commit with basic web app files.

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_from_directory
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    #return 'Hello World!'
+    return send_from_directory('templates', 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
