8ab0baba742343b31e9cae7712447568e2f68ea5
rob144 <rob144@users.noreply.github.com>
2015-03-21 22:21:58 +0800

    Handle request for contact form submit and added js scroll animation for sections.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, send_from_directory
+from flask import Flask, request, send_from_directory, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    #return 'Hello World!'
+def main():
     return send_from_directory('templates', 'index.html')
+
+@app.route('/contact-msg', methods=['POST'])
+def contact_msg():
+    return render_template('contact-msg.html', contact_name=request.form['contact-name'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=81)
