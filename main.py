cebcf5aa84355cbfe70ba04e13268662b8b11acb
rob144 <rob144@users.noreply.github.com>
2015-03-25 18:06:46 +0800

    Added redirect behaviour for contact form submission.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory, render_template
+from flask import Flask, request, url_for, redirect, send_from_directory, render_template
 import sqlite3 as lite
 
 app = Flask(__name__)
@@ -17,29 +17,35 @@ with con:
             contact_message TEXT)"""
     )
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def main():
-    return send_from_directory('templates', 'index.html')
-
-@app.route('/contact-msg', methods=['POST'])
-def contact_msg():
-    contact_name = request.form['contact-name']
-    contact_email = request.form['contact-email']
-    contact_telephone = request.form['contact-telephone']
-    contact_message = request.form['contact-message']
+    if request.method != 'POST':
+        return render_template('index.html')
+    else: 
+        contact_name = request.form['contact-name']
+        contact_email = request.form['contact-email']
+        contact_telephone = request.form['contact-telephone']
+        contact_message = request.form['contact-message']
     
-    if(len(contact_message) > 3):
-        con = get_db_con()
-        with con:
-            con.cursor().execute(
-                """INSERT INTO messages 
-                    (contact_name, contact_email, contact_telephone, contact_message)
-                    VALUES (?, ?, ?, ?)""",
-                    (contact_name, contact_email, contact_telephone, contact_message)
-            )
-        return render_template('contact-msg.html', contact_name=request.form['contact-name'])
+        if(len(contact_message) > 3):
+            con = get_db_con()
+            with con:
+                con.cursor().execute(
+                    """INSERT INTO messages 
+                        (contact_name, contact_email, contact_telephone, contact_message)
+                        VALUES (?, ?, ?, ?)""",
+                        (contact_name, contact_email, contact_telephone, contact_message)
+                )
+            return redirect(url_for('contact', contact_name=contact_name))
+        else:
+            return render_template('index.html', error='Message was empty.')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    if(request.referrer):
+        return render_template('contact-msg.html',contact_name=request.args.get('contact_name'))
     else:
-        return render_template('index.html', error='Message was empty.')
+        return redirect(url_for('main'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=81)
